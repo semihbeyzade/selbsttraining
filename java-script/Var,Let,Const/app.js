a461117e683e586660937976a7b1ff70d1837c98
Semih Beyzade Örscelik <semihb.orscelik0@gmail.com>
2022-01-23 17:20:29 +0100

    new exercise

---
+++ java-script/Var,Let,Const/app.js
@@ -0,0 +1,35 @@
+// var
+
+// var name = "Semih Beyzade";
+
+// name = "Sevde"
+
+// console.log(name);
+
+// name = "Sevde";
+
+// console.log(name);
+
+// let 
+
+// let a,b;
+
+// a = 10;
+// b = 20;
+
+// console.log(a+b);
+
+// const
+
+// const name = "Semih Beyzade"
+
+// console.log(name);
+
+// name = "Sevde";
+
+// console.log(name);
+
+const a = [1,2,3,4,5];
+a.push(6);
+
+console.log(a);
